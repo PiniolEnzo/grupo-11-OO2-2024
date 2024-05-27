553c7655bab3f93a89c71a9016de95b81a55da00
juanpi8345 <juanpipre@outlook.com>
2024-05-27 20:24:26 -0300

    Se agrega repositorio de venta_producto

---
+++ tpc/src/main/java/com/grupo11tpc/tpc/repositories/IProductSaleRepository.java
@@ -0,0 +1,13 @@
+package com.grupo11tpc.tpc.repositories;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.grupo11tpc.tpc.entities.ProductSale;
+
+@Repository("productSaleRepository")
+public interface IProductSaleRepository extends JpaRepository<ProductSale, Serializable> {
+
+}
