dfca201687498b894d579b1412fe576e800a0533
juanpi8345 <juanpipre@outlook.com>
2024-06-14 17:58:50 -0300

    Creacion de controlador y nuevas rutas para stock

---
+++ tpc/src/main/java/com/grupo11tpc/tpc/controllers/ProductEntryController.java
@@ -0,0 +1,47 @@
+package com.grupo11tpc.tpc.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.grupo11tpc.tpc.dtos.ProductDTO;
+import com.grupo11tpc.tpc.dtos.ProductEntryDTO;
+import com.grupo11tpc.tpc.helpers.ViewRouteHelper;
+import com.grupo11tpc.tpc.services.IProductEntryService;
+import com.grupo11tpc.tpc.services.IProductService;
+
+@Controller
+@RequestMapping("")
+public class ProductEntryController {
+	
+	private IProductService productService;
+	private IProductEntryService productEntryService;
+	
+	private ModelMapper modelMapper = new ModelMapper();
+	
+	public ProductEntryController(IProductService productService, IProductEntryService productEntryService) {
+		super();
+		this.productService = productService;
+		this.productEntryService = productEntryService;
+	}
+
+	@GetMapping("")
+	public ModelAndView index() {
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.PRODUCT_ENTRY_INDEX);
+		//Se envian todos los productos para dar a elegir sobre cual hacer el alta de stock
+		mAV.addObject("products", productService.getAll());
+		return mAV;
+	}
+
+	@PostMapping("/create")
+	public RedirectView create(@ModelAttribute("stock") ProductEntryDTO productEntryDto) {
+		productEntryService.saveProductEntry(modelMapper.map(productEntryDto, ProductEntryDTO.class));
+		return new RedirectView(ViewRouteHelper.ROUTE);
+	}
+
+}
--- tpc/src/main/java/com/grupo11tpc/tpc/helpers/ViewRouteHelper.java
+++ tpc/src/main/java/com/grupo11tpc/tpc/helpers/ViewRouteHelper.java
@@ -6,6 +6,10 @@ public class ViewRouteHelper {
 	public final static String PRODUCT_INDEX = "product/index";
 	public final static String PRODUCT_NEW = "product/new";
 	public final static String PRODUCT_UPDATE = "product/update";
+	
+	//LOTE
+	public final static String PRODUCT_ENTRY_INDEX = "stock/index";
+	public final static String PRODUCT_ENTRY_CREATE = "stock/create";
 
 	/**** Redirects ****/
 	public final static String ROUTE = "/index";
